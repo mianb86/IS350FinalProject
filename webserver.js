7bf8bdcddb863d60bd77daf0a986448b1a7897fd
mianb86 <mianb86@uwosh.edu>
2020-12-15 00:19:26 -0600

    Intial Commit

---
+++ webserver.js
@@ -0,0 +1,25 @@
+var http = require('http')
+var fs = require('fs')
+const PORT = process.env.PORT || 5000;
+
+http.createServer(function (req, res) {
+    let route = req.url.replace('/', '')
+
+    let request_url = route === '' || route === '/' ? 'index.html' : route
+
+    console.log(request_url)
+
+    fs.exists(request_url, (exist) => {
+        if (Boolean(exist) === false) {
+            res.writeHead(404, {'content-type': 'text/html'})
+            res.end('Page Not Found')
+            return null
+        }
+
+        fs.readFile(request_url, function (err, data) {
+            res.writeHead(200)
+            res.write(data)
+            res.end()
+        })
+    })
+}).listen(PORT);
